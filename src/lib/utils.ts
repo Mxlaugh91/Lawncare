8614b9c46343f5202f86cd52319fe284e9963651
Mxlaugh91 <fredrikaleksander91@gmail.com>
2025-06-05 23:21:55 +0200

    Add getISOWeekDates function to utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -16,3 +16,25 @@ export function getISOWeekNumber(date: Date): number {
   }
   return 1 + Math.ceil((firstThursday - target.valueOf()) / 604800000);
 }
+
+export function getISOWeekDates(weekNumber: number, year: number = new Date().getFullYear()) {
+  // Create a date object for January 1st of the given year
+  const januaryFirst = new Date(year, 0, 1);
+  
+  // Get the Thursday in week 1 and add the appropriate number of weeks
+  const firstThursday = new Date(januaryFirst);
+  firstThursday.setDate(januaryFirst.getDate() + (4 - januaryFirst.getDay()) + ((weekNumber - 1) * 7));
+  
+  // Get Monday by subtracting 3 days from Thursday
+  const weekStart = new Date(firstThursday);
+  weekStart.setDate(firstThursday.getDate() - 3);
+  
+  // Get Sunday by adding 6 days to Monday
+  const weekEnd = new Date(weekStart);
+  weekEnd.setDate(weekStart.getDate() + 6);
+  
+  return {
+    start: weekStart,
+    end: weekEnd
+  };
+}
