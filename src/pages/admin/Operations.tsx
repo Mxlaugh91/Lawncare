4e88ec86562fee064aaf0a08940b9054017d58eb
Mxlaugh91 <fredrikaleksander91@gmail.com>
2025-06-09 11:36:54 +0200

    Updated Operations.tsx

--- src/pages/admin/Operations.tsx
+++ src/pages/admin/Operations.tsx
@@ -75,7 +75,7 @@ const WeekSelector = ({ selectedWeek, onWeekChange }: {
   return (
     <Popover>
       <PopoverTrigger asChild>
-        <Button variant="outline" className="w-[150px] justify-start">
+        <Button variant="outline" className="w-[170px] justify-start">
           <Calendar className="mr-2 h-4 w-4" />
           Uke {selectedWeek} ({formatDate(start)} - {formatDate(end)})
         </Button>
