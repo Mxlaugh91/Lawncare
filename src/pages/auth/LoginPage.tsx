f3bc8bf180c86281eaf972032f27284deab3d413
Mxlaugh91 <fredrikaleksander91@gmail.com>
2025-06-09 21:38:25 +0200

    Updated LoginPage.tsx

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -61,7 +61,7 @@ const LoginPage = () => {
           </div>
           <CardTitle className="text-2xl font-bold">Logg inn</CardTitle>
           <CardDescription>
-            Logg inn for å få tilgang
+            Logg inn for å få tilgang til plenpilot
           </CardDescription>
         </CardHeader>
         <CardContent>
