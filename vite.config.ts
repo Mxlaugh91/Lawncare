de01886b70a938dafa52ea4663228aa923b2534b
Mxlaugh91 <fredrikaleksander91@gmail.com>
2025-06-09 14:00:36 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/Lawncare/', // <--- DENNE LINJEN MANGLER HOS DEG
   plugins: [react()],
   resolve: {
     alias: {
