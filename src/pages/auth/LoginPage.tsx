f6af73dabc48f3d77fc44e1e5a7d3ab675b27fc6
Mxlaugh91 <fredrikaleksander91@gmail.com>
2025-06-11 23:27:58 +0200

    Updated LoginPage.tsx

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -61,7 +61,7 @@ const LoginPage = () => {
           </div>
           <CardTitle className="text-2xl font-bold">Logg inn</CardTitle>
           <CardDescription>
-            Logg inn for å få tilgang til PlenPilots bruker
+            Logg inn nisse
           </CardDescription>
         </CardHeader>
         <CardContent>
