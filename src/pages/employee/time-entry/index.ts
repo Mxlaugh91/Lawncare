7d8dc2a544aab1fc4359261b46bd6dbab465f622
Mxlaugh91 <fredrikaleksander91@gmail.com>
2025-06-13 22:27:35 +0200

    Updated index.ts

--- src/pages/employee/time-entry/index.ts
+++ src/pages/employee/time-entry/index.ts
@@ -1,3 +1,3 @@
 export { EmployeeSelector } from './EmployeeSelector';
 export { LocationSelector } from './LocationSelector';
-export { default as EmployeeTimeEntry } from './EmployeeTimeEntry';
+export { default as EmployeeTimeEntry } from './TimeEntry';
