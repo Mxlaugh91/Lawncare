b9f706d04813baf113587a1a5fa1546a8b4414a6
Mxlaugh91 <fredrikaleksander91@gmail.com>
2025-06-13 22:34:03 +0200

    Updated index.ts

--- src/pages/employee/time-entry/index.ts
+++ src/pages/employee/time-entry/index.ts
@@ -1,4 +1,4 @@
 // Enhanced components index with PWA optimizations
 export { EmployeeSelector } from './EmployeeSelector';
 export { LocationSelector } from './LocationSelector';
-export { default as TimeEntry } from './TimeEntry';
+export { default as TimeEntry } from '../TimeEntry';
